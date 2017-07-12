c8a9719e07ea3f391f8f2834c2c9e5baf6f8a9e2
Daniel <dlopez6877@gmail.com>
2017-07-12 03:37:32 -0700

    follow lesson but have undefined  key in marketplace

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
-import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class AlbumService {
+  albums: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(
+    private database: AngularFireDatabase
+  ) {
+  this.albums = database.list('albums');
+ }
 
-  getAlbums(): Album[] {
-    return ALBUMS;
+  getAlbums() {
+    return this.albums;
   }
 
-  getAlbumById(albumId: number): Album {
-    return ALBUMS.filter(album => album.id === albumId)[0]
+  addAlbum(newAlbum: Album) {
+    this.albums.push(newAlbum);
+  }
+
+  getAlbumById(albumId: string) {
+    return this.database.object('albums/' + albumId);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { StorePipePipe } from './store-pipe.pipe';
@@ -10,6 +9,17 @@ import { AboutComponent } from './about/about.component';
 import { routing } from './app.routing';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AdminComponent } from './admin/admin.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -18,13 +28,16 @@ import { AlbumDetailComponent } from './album-detail/album-detail.component';
     StorePipePipe,
     AboutComponent,
     MarketplaceComponent,
-    AlbumDetailComponent
+    AlbumDetailComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
